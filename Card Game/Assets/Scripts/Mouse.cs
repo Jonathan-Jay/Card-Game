dd77d9ddde302bbd5c146d4be527f32aaca39bd8
Matt N <northmatt1@gmail.com>
2022-02-07 15:08:25 -0500

    Basic Hover
    -Remember to enable in script, disabled by default

--- Card Game/Assets/Scripts/Mouse.cs
+++ Card Game/Assets/Scripts/Mouse.cs
@@ -7,6 +7,12 @@ public class Mouse : MonoBehaviour {
     public float maxDist = 0f;
     public float vertOffset = 0f;
 	public int grabbedMask;
+	public bool doHover = false;
+
+	bool isHovering = false;
+	GameObject hoverObj;
+	Vector3 hoverObjOrigPos = Vector3.zero;
+	Vector3 hoverObjVel = Vector3.zero;
 	int tempLayer = -1;
 
     private Camera cam;
@@ -23,6 +29,7 @@ public class Mouse : MonoBehaviour {
 
         if (Physics.Raycast(rayInfo, out rayHitInfo, maxDist, mask)) {
             mouseObject.position = rayHitInfo.point + Vector3.up * vertOffset;
+			if (doHover) Hover(rayHitInfo);
 
 			if (Input.GetMouseButtonDown(0)) {
 				//first check if we wanna do smt
@@ -81,4 +88,35 @@ public class Mouse : MonoBehaviour {
 			}
         }
     }
+
+	bool Hover(RaycastHit rayHitInfo, bool tryDehover = true) {
+		if (tryDehover && !DeHover(rayHitInfo)) return false;			//You cannot hover an object when dehovering another object
+		if (rayHitInfo.transform.gameObject.layer != 6) return false;	//Dont hover a non-card
+		if (!isHovering) {
+			isHovering = true;
+			hoverObj = rayHitInfo.transform.gameObject;
+			hoverObjOrigPos = hoverObj.transform.position;
+		}
+
+		Vector3 targetPos = hoverObjOrigPos + hoverObj.transform.up * 0.1f;
+		hoverObj.transform.position = Vector3.SmoothDamp(hoverObj.transform.position, targetPos, ref hoverObjVel, 0.1f, 2f, Time.deltaTime);
+
+		return true;	//I am hover boi
+	}
+
+	bool DeHover(RaycastHit rayHitInfo) {
+		if (!isHovering || rayHitInfo.transform.gameObject == hoverObj) return true;	//No Obj was previously hovered OR still hovering over same Obj
+
+		if (Vector3.Distance(hoverObj.transform.position, hoverObjOrigPos) < 0.0001f ) {
+			hoverObj.transform.position = hoverObjOrigPos;
+			isHovering = false;
+			hoverObj = null;
+			hoverObjOrigPos = Vector3.zero;
+			return true;		//fully "dehovered" Obj
+		}
+
+		hoverObj.transform.position = Vector3.SmoothDamp(hoverObj.transform.position, hoverObjOrigPos, ref hoverObjVel, 0.1f, 2f, Time.deltaTime);
+		
+		return false;		//In process of "dehovering" Obj
+	}
 }
