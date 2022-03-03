34c534f537932b5b0b8976983fda93ebb9a1b6e0
Matt N <northmatt1@gmail.com>
2022-03-03 17:38:48 -0500

    Card Select Animation
    Fixes:
    -Fixed desync issue with cards original position
    -Cards not in hand object wont be animated
    
    Bugs?:
    -Potential errors when taking cards out of hand object (full selection)
    -Potential residual info left in a list storing card information when taking cards out of hand (full selection)

--- Card Game/Assets/Scripts/Mouse.cs
+++ Card Game/Assets/Scripts/Mouse.cs
@@ -9,16 +9,19 @@ public class Mouse : MonoBehaviour {
 	public int grabbedMask;
 	public bool doHover = false;
 
-	bool isHovering = false;
-	GameObject hoverObj;
-	Vector3 hoverObjOrigPos = Vector3.zero;
+	private struct hoverObj {
+		public GameObject gameObject;
+		public Vector3 origPos;
+	}
+	int isHoveringIndex = -1;
+	List<hoverObj> hoverObjs = new List<hoverObj>();
 	int tempLayer = -1;
 
     private Camera cam;
     // Start is called before the first frame update
     void Start() {
         cam = GetComponent<Camera>();
-    }
+	}
 
     // Update is called once per frame
     void Update() {
@@ -90,26 +93,39 @@ public class Mouse : MonoBehaviour {
 
 	void HoverManagement(RaycastHit rayHitInfo) {
 		//Can only dehover if you're hovering smth and if raycast has different output then saved input
-		if (isHovering && rayHitInfo.transform.gameObject != hoverObj) StartCoroutine(DeActivateHover(rayHitInfo));
-		//Can only animate the hovering if you arent hovering smth and if layer is card
-		if (!isHovering && rayHitInfo.transform.gameObject.layer == 6) StartCoroutine(ActivateHover(rayHitInfo));
+		if (IsHovering() && rayHitInfo.transform.gameObject != hoverObjs[isHoveringIndex].gameObject) StartCoroutine(DeActivateHover());
+		//Can only animate the hovering if you arent hovering smth, if layer is card, and if parent is the hand
+		if (!IsHovering() && rayHitInfo.transform.gameObject.layer == 6 && rayHitInfo.transform.parent && rayHitInfo.transform.parent.CompareTag("Player Hand")) StartCoroutine(ActivateHover(rayHitInfo));
+	}
+
+	bool IsHovering() {
+		return isHoveringIndex > -1;
 	}
 
 	IEnumerator ActivateHover(RaycastHit rayHitInfo) {
-		isHovering = true;
-		hoverObj = rayHitInfo.transform.gameObject;
-		hoverObjOrigPos = hoverObj.transform.localPosition;
+		bool foundMs = false;
+		hoverObj ms;
+		ms.gameObject = rayHitInfo.transform.gameObject;
+		ms.origPos = ms.gameObject.transform.localPosition;
 
-		GameObject tempHoverObj = hoverObj;
-		Vector3 tempHoverObjOffset = tempHoverObj.transform.localRotation * new Vector3(0f, 0.066f, 0.1f);
-		Vector3 targetPos = hoverObjOrigPos + tempHoverObjOffset;
+        for (isHoveringIndex = 0; isHoveringIndex < hoverObjs.Count; ++isHoveringIndex)
+			if (hoverObjs[isHoveringIndex].gameObject == ms.gameObject) {
+				foundMs = true;
+				ms.origPos = hoverObjs[isHoveringIndex].origPos;
+				break;
+			}
+
+		if (!foundMs)
+			hoverObjs.Add(ms);
+
+		GameObject tempHoverObj = ms.gameObject;
+		Vector3 targetPos = ms.origPos + tempHoverObj.transform.localRotation * new Vector3(0f, 0.066f, 0.1f);
 		Vector3 hoverObjVel = Vector3.zero;
 		bool tempLoop = true;
 
         while (tempLoop) {
-			if (tempHoverObj != hoverObj)
+			if (!IsHovering() || IsHovering() && tempHoverObj != hoverObjs[isHoveringIndex].gameObject)
 				break;
-			targetPos = hoverObjOrigPos + tempHoverObjOffset;
 			tempLoop = Vector3.Distance(tempHoverObj.transform.localPosition, targetPos) >= 0.01f;
 
 			tempHoverObj.transform.localPosition = Vector3.SmoothDamp(tempHoverObj.transform.localPosition, targetPos, ref hoverObjVel, 0.1f, 2f, Time.deltaTime);
@@ -121,20 +137,22 @@ public class Mouse : MonoBehaviour {
 			tempHoverObj.transform.localPosition = targetPos;
 
 		//Debug.Log("ActivateHover: " + hoverObjOrigPos);
+		Debug.Log("AcHoverObjsAmt: " + hoverObjs.Count);
 	}
 
-	IEnumerator DeActivateHover(RaycastHit rayHitInfo) {
-		GameObject tempHoverObj = hoverObj;
-		Vector3 targetPos = hoverObjOrigPos;
+	IEnumerator DeActivateHover() {
+		GameObject tempHoverObj = hoverObjs[isHoveringIndex].gameObject;
+		Vector3 targetPos = hoverObjs[isHoveringIndex].origPos;
 		Vector3 hoverObjVel = Vector3.zero;
 		bool tempLoop = true;
 
-		isHovering = false;
-		hoverObj = null;
-		hoverObjOrigPos = Vector3.zero;
+		isHoveringIndex = -1;
+		hoverObj ms;
+		ms.gameObject = tempHoverObj;
+		ms.origPos = targetPos;
 
 		while (tempLoop) {
-			if (tempHoverObj == hoverObj)
+			if (IsHovering() && tempHoverObj == hoverObjs[isHoveringIndex].gameObject)
 				break;
 			tempLoop = Vector3.Distance(tempHoverObj.transform.localPosition, targetPos) >= 0.01f;
 
@@ -143,11 +161,15 @@ public class Mouse : MonoBehaviour {
 			yield return null;
 		}
 
-		if (!tempLoop)
+		if (!tempLoop) {
 			tempHoverObj.transform.localPosition = targetPos;
-		else
-			hoverObjOrigPos = targetPos;    //In process of dehovering object has been hovered again thus OriginalPos was set incorrectly
+			//indexes can change if other DeActivator deletes it, only gets deleted if loop exited "cleanly" as "unclean" exits means it needs it
+			int indexToRemove = hoverObjs.FindIndex(x => x.gameObject == ms.gameObject);
+			hoverObjs.RemoveAt(indexToRemove);
+			if (isHoveringIndex >= indexToRemove && IsHovering()) --isHoveringIndex;
+		}
 
 		//Debug.Log("DeActivateHover: " + targetPos);
+		Debug.Log("DeHoverObjsAmt: " + hoverObjs.Count);
 	}
 }
