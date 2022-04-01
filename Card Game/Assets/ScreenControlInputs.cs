d171626f724238d455814a43aa01661a18f2b86a
Jonathan Jay <jonathan.jay@ontariotech.net>
2022-04-01 16:57:29 -0400

    pretrailerbuild

---
+++ Card Game/Assets/ScreenControlInputs.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenControlInputs : MonoBehaviour
+{
+	public KeyCode toggleWindowed = KeyCode.Backslash;
+
+	void Update(){
+		if (Input.GetKeyDown(toggleWindowed)) {
+			Screen.fullScreen = !Screen.fullScreen;
+		}
+	}
+}
